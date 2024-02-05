e5bcdb2f4a69dd9411d93bcc3af8a79091281b95
Kadalmesir30 <155425923+Kadalmesir30@users.noreply.github.com>
2024-02-05 23:01:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('optionButton').addEventListener('mouseenter', function() {
+    document.getElementById('loginContainer').style.backgroundColor = '#2980b9';
+  });
+  
+  document.getElementById('optionButton').addEventListener('mouseleave', function() {
+    document.getElementById('loginContainer').style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+  });
+  
